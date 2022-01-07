53e454d347b88455874e0e535a6ac9693e2c8cfc
Steven Winters <82731990+winters24-tech@users.noreply.github.com>
2022-01-06 19:14:58 -0500

    Add files via upload

---
+++ Python-block-letters.py
@@ -0,0 +1,12 @@
+print(" SSS    W   W")
+print("S   S   W   W")
+print("S       W   W")
+print(" SSS    W W W")
+print("    S   WW WW")
+print("S   S   WW WW")
+print(" SSS    W   W")
+
+
+Name = "Steven Winters"
+Fun_fact = "I enjoy coding and I would love to make this a career"
+print(Name + Fun_fact)
